e766f5b9fef5993b5ad8a3523944b21d37d23cc1
tannerj <codingfrogs@yahoo.com>
2021-03-22 13:31:15 +0000

    Add script for global variables.

---
+++ sections/basics/scripts/basics-11-export.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Variables with export"
+
+#Local variables
+local_var="Local to the parent"
+export exported_var="Exported in the parent"
+
+
+#Display a variables
+echo "Parent: local_var    = " $local_var
+echo "Parent: exported_var = " $exported_var
+
+./basics-11.1-export-child.sh
---
+++ sections/basics/scripts/basics-11.1-export-child.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Child script: Using the parent variables
+
+#Display a variables
+echo "Child: local_var    = " $local_var
+echo "Child: exported_var = " $exported_var
