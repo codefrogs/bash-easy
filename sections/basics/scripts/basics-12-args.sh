ff8e4d5dbf358552a987d7da0390ac89726d4a05
tannerj <codingfrogs@yahoo.com>
2021-03-22 14:04:53 +0000

    Add minimal argument passing.

---
+++ sections/basics/scripts/basics-12-args.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Number of parameters: $#"
+
+
+# Look at the parameters!
+echo "\$0 = $0"  # The script name
+echo "\$1 = $1"  # First parameter!
+echo "\$2 = $2"  # Second...and so on!
+echo "\$3 = $3"
+echo "\$4 = $4"
+echo "\$5 = $5"
