2ee6bface2e0e3aa1ca36a525ab61f2a0a06cd67
tannerj <codingfrogs@yahoo.com>
2021-04-20 13:46:10 +0100

    Add 'command subtitution' examples.

---
+++ sections/basics/scripts/basics-16-command-subtitution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Bash-easy by Codefrogs"
+# Using 'command substitution' to set a variable with output from a command.
+
+# Running a command, and setting a variable
+iam="$(whoami)"
+
+echo "User is: $iam"
+
+#Get all processes
+processes="$(ps -a)"
+echo "$processes"
