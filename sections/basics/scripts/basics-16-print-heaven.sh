54d54de8ca9614a3a72eb2e59ed835ac5d34a644
tannerj <codingfrogs@yahoo.com>
2021-05-10 15:26:49 +0100

    Add print examples.

---
+++ sections/basics/scripts/basics-16-print-heaven.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Bash-easy by Codefrogs"
+# Using 'print'
+
+#Test with escape characters
+some_text="I think\nTherefore I am.\nSocrates"
+
+#This works fine (in a real terminal)
+printf "$some_text"
+echo
+printf "Long line:"
+printf "This is a rather long line, \
+but we can split the text for readability \
+without breaking it up when displaying it.\n"
+echo
+#Print without newlines
+printf "Hello "
+printf "my "
+printf "name "
+printf "is "
+printf "Plato.\n"
+echo
+
+# Print with multiple lines
+echo "Display processes"
+val=$(ps -a)  # This has at least two lines of text
+printf $val     # Doesn't work properly
+echo "***Trying again!***"
+printf "$val"  # Much better
+echo "***Now with a newline***"
+printf "%s\n" "$val"
