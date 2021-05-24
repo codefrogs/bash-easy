80479bfec655b815d049f9d6ed77c292263a6b5a
tannerj <codingfrogs@yahoo.com>
2021-05-24 12:21:24 +0100

    Add example showing different forms to testing.

---
+++ sections/basics/scripts/basics-27-test-n-brackets.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Bash-easy by Codefrogs
+#
+# Lets explore testing. Below are the three forms:
+#
+#  - test [expression]  - test method.
+#  -      [expression]  - single bracket test (easier to understand)
+#  -     [[expression]] - double bracket test (with some extra features)
+# The expressions contain tests such as equality, inequality, or tests on files.
+#
+# See: `help test`
+#
+
+# Set up some variables to test with.
+hello="Hello World!"
+greeting=$hello
+filename="$0"  # get this file's name.
+
+# Using 'test'
+if test -e $filename; then
+  echo "1: Check file exists using 'test': $filename"
+fi
+# Again with single brackets '[]'.
+if [ -e $filename ]; then
+  echo "2: Check file exists using '[]'  : $filename"
+fi
+# Same again but with double backets '[[]]'.
+if [[ -e $filename ]]; then
+  echo "3: Check file exists using '[[]]': $filename"
+fi
+# All the same!
+
+# Check string equality.
+# Notice that we use double quotes here because the string contains a space.
+# Try it without to see what happens.
+if test "$hello"=="$greeting"; then
+  echo "4: String match found with 'test'."
+fi
+if    [ "$hello"=="$greeting" ]; then
+  echo "5: String match found with '[]'."
+fi
+if   [[ "$hello"=="$greeting" ]]; then
+  echo "6: String match found with '[[]]'."
+fi
+if   [[  $hello == $greeting ]]; then
+  echo "7: String match found with '[[]]' but without quotes around variables."
+fi
+
+# With double brackets we can use pattern matching and regular expressions!
+# Patter matching
+if [[ $filename=*.sh ]]; then
+  echo "8: Pattern *.sh match with '[[]]': $filename"
+fi
+# Regular expression
+if [[ $filename =~ basics-[[:digit:]]{2}-(-|[[:alpha:]])+\.sh ]]; then
+  echo "9: Regex        match with '[[]]': $filename"
+fi
