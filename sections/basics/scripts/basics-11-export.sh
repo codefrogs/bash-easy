88dddadbc3d5a44fa7b4a42aa299c0df34e3347c
tannerj <codingfrogs@yahoo.com>
2021-05-25 14:32:01 +0100

    Tidy headings

--- sections/basics/scripts/basics-11-export.sh
+++ sections/basics/scripts/basics-11-export.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Bash-easy by Codefrogs
 echo "Bash-easy by Codefrogs"
-
+echo
 echo "Variables with export"
-
+echo "---------------------"
+echo
 #Local variables
 local_var="Local to the parent"
 export exported_var="Exported in the parent"
@@ -12,5 +13,5 @@ export exported_var="Exported in the parent"
 #Display a variables
 echo "Parent: local_var    = " $local_var
 echo "Parent: exported_var = " $exported_var
-
+echo
 ./basics-11.1-export-child.sh
--- sections/basics/scripts/basics-11.1-export-child.sh
+++ sections/basics/scripts/basics-11.1-export-child.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Bash-easy by Codefrogs
-echo "Bash-easy by Codefrogs"
 
 #Child script: Using the parent variables
 
