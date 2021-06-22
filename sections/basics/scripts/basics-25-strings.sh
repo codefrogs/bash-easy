78ad53046af7ed35f4dedc41ac9f48fdff1ac9d9
tannerj <codingfrogs@yahoo.com>
2021-06-22 11:50:27 +0100

    Expand string examples with: length, default values.

--- sections/basics/scripts/basics-25-strings.sh
+++ sections/basics/scripts/basics-25-strings.sh
@@ -2,10 +2,26 @@
 # Bash-easy by Codefrogs
 echo "Bash-easy by Codefrogs"
 echo
-echo "String chopping from the left."
+echo "Strings"
+echo "-------"
 echo
 
 # Getting the string length using '#'
 # We will see '#' again when using 'arrays'.
-length=${#root}
-echo "The length of the word $root is: $length"
+path=$(pwd)
+length=${#path}
+echo "1: path = $path"
+echo '1: Using: length=${#path}'
+echo "1: Length of '\$path': $length"
+echo
+
+user_name=${user:-"anonymous"}
+echo "2: Undefined '\$user' example."
+echo "2: Using '\${user:-\"anonymous\"}'"
+echo "2: User name: $user_name"
+echo "2: Note: User variable '\$user' is not assigned: $user"
+echo
+
+user_name=${user:="Root"}
+echo "3: Using '\${user:=\"Root\"}': User name: $user_name"
+echo "3: Note: User variables '\$user' is assigned: $user"
