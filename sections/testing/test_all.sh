fdfc81465c6c60d80470f81e18e6b7ae4ac7c2ee
tannerj <codingfrogs@yahoo.com>
2021-06-25 15:21:41 +0100

    feat: Add code to run all tests in project.

--- sections/testing/test_all.sh
+++ sections/testing/test_all.sh
@@ -1,2 +1,19 @@
+#!/bin/bash
+# Bash-easy by Codefrogs
+#
 # Runs all tests.
-# TODO: implement
+# Goes through all sections running all the tests.
+#
+
+echo "---------"
+echo "run_tests"
+echo "---------"
+echo
+curr_dir=$(pwd)  # current directory
+_lib=$curr_dir/lib
+export _lib
+
+source $_lib/testing-library.sh
+
+run_tests "../"
+test_summary
