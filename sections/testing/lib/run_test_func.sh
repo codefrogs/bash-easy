6d166d6c695a2e3d16301951b3695e9f9fe7421f
tannerj <codingfrogs@yahoo.com>
2021-06-25 15:15:13 +0100

    docs: Clarify / Simplify script arguments

--- sections/testing/lib/run_test_func.sh
+++ sections/testing/lib/run_test_func.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Bash-easy by Codefrogs
-
+#
 # Runs a test function in a file.
-# param1: test get_filename.
-# param2: the test function to call.
+# param1: test filename.
+# param2: test function to call.
 # returns: the exit code 0 for success, false otherwise.
 set -u  # Exit on unknown variables
 
@@ -14,6 +14,6 @@ test_func=$2
 source $test_file  # source the test functions
 $test_func         # run the function
 
-# Verify the return code
+# Return exit code
 code=$?
 exit $code
