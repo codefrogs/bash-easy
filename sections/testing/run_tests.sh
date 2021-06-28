aa623b9988dc767d7a783243a54711cfe9d22bb7
tannerj <codingfrogs@yahoo.com>
2021-06-28 22:40:42 +0100

    feat: give other scripts a 'root' directory variable

--- sections/testing/run_tests.sh
+++ sections/testing/run_tests.sh
@@ -4,12 +4,16 @@
 # Runs all tests.
 # Goes through all sections running all the tests.
 #
+# TODO: allow option to give a path to the script.
 
 echo "---------"
 echo "run_tests"
 echo "---------"
 echo
 curr_dir=$(pwd)  # current directory
+_root=${curr_dir%/*}  # // sections directory
+export _root
+
 _lib=$curr_dir/lib
 export _lib
 
