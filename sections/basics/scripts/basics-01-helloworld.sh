13a06bca1b32c36a202e561f7c60eb835ee6c1ec
tannerj <codingfrogs@yahoo.com>
2021-06-28 22:37:55 +0100

    docs: add reference to bash homepage

--- sections/basics/scripts/basics-01-helloworld.sh
+++ sections/basics/scripts/basics-01-helloworld.sh
@@ -2,6 +2,7 @@
 # Bash-easy by Codefrogs
 # A comment: This line above should already be the very first line!
 # Another comment!
+# see: www.gnu.org/software/bash
 #
 echo "Bash-easy by Codefrogs"
 echo "Hello World!"
