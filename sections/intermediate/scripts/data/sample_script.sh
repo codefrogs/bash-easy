b275a8d4b7c286c68738d1fc7f3c16266f134c6b
tannerj <codingfrogs@yahoo.com>
2021-07-01 10:28:46 +0100

    feat: Add example that uses return code from script

--- sections/intermediate/scripts/data/sample_script.sh
+++ sections/intermediate/scripts/data/sample_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Bash-easy by Codefrogs
-# A sample script for 'inter-xx-execute script'
-echo -e "1: Connected.\n2: Found record:122.\n3: Disconnected\n"
+# A sample script for 'inter-xx-execute-script'
+echo "1: Connected to database Milow17."
+echo "2: Found customer: ANB-2529"
+echo -e "3: Disconnecting..."
+sleep 1
+echo "Timed out!" >& 2
 exit 122
--- sections/intermediate/scripts/inter-02-execute-script.sh
+++ sections/intermediate/scripts/inter-02-execute-script.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 # Bash-easy by Codefrogs
 # Running another script, and using the return code.
+echo "Bash-easy by Codefrogs"
+echo
 
 echo "A: Running script: data/sample_script.sh"
 echo
+
+# Run a script
 data/sample_script.sh
+
+# Get the exit result
 result=$?
-echo "B: Result: $result"
+
+# Display the result (0->SUCCESS, otherwise failure)
+if (( result!=0 )); then
+  echo "Error: $result"
+else
+  echo "X: OK!"
+fi
