91b3ac00d0abc9d437e49909f37d675d6f5523c0
tannerj <codingfrogs@yahoo.com>
2021-07-05 14:48:33 +0100

    fix: Rename calls to 'simple' scripts to 'options' scripts.

--- sections/intermediate/scripts/inter-05-options.sh
+++ sections/intermediate/scripts/inter-05-options.sh
@@ -7,46 +7,51 @@ echo
 
 
 # Show the help
-echo "./inter-05.1-simple.sh -h"
-      ./inter-05.1-simple.sh -h
+echo "./inter-05.1-options.sh -h"
+      ./inter-05.1-options.sh -h
 echo
 
 # Show the help
-echo "./inter-05.1-simple.sh --help"
-      ./inter-05.1-simple.sh --help
+echo "./inter-05.1-options.sh --help"
+      ./inter-05.1-options.sh --help
 echo
 
 # Show the version
-echo "./inter-05.1-simple.sh --version"
-      ./inter-05.1-simple.sh --version
+echo "./inter-05.1-options.sh --version"
+      ./inter-05.1-options.sh --version
 echo
 
 # Show the information
-echo "./inter-05.1-simple.sh --info"
-      ./inter-05.1-simple.sh --info
+echo "./inter-05.1-options.sh --info"
+      ./inter-05.1-options.sh --info
 echo
 
 # Show the version, and information
-echo "./inter-05.1-simple.sh -v -i"
-      ./inter-05.1-simple.sh -v -i
+echo "./inter-05.1-options.sh -v -i"
+      ./inter-05.1-options.sh -v -i
 echo
 
 # Run with info and give two arguments
-echo "./inter-05.1-simple.sh -i apple cake"
-      ./inter-05.1-simple.sh -i apple cake
+echo "./inter-05.1-options.sh -i apple cake"
+      ./inter-05.1-options.sh -i apple cake
+echo
+
+# An option with a parameter
+echo "./inter-05.1-options.sh -u tanner"
+      ./inter-05.1-options.sh -u tanner
 echo
 
 # Run with info and give too many arguments
-echo "./inter-05.1-simple.sh engine oil monkey"
-      ./inter-05.1-simple.sh engine oil monkey
+echo "./inter-05.1-options.sh engine oil monkey"
+      ./inter-05.1-options.sh engine oil monkey
 echo
 
 # Give an unknown option
-echo "./inter-05.1-simple.sh -u"
-     ./inter-05.1-simple.sh -u
+echo "./inter-05.1-options.sh -q"
+      ./inter-05.1-options.sh -q
 echo
 
-# Try things in wrong order
-echo "./inter-05.1-simple.sh captain wood -v"
-      ./inter-05.1-simple.sh captain wood -v
+# Putting options after positional arguments
+echo "./inter-05.1-options.sh beef stew -v"
+      ./inter-05.1-options.sh beef stew -v
 echo
