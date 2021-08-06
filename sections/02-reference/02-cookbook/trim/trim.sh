6ded05635b775e889defd0c32ad5efd55d6631e4
tannerj <codingfrogs@yahoo.com>
2021-08-06 22:00:36 +0100

    feat: Add trim 'function' example

---
+++ sections/02-reference/02-cookbook/trim/trim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Bash-easy by Codefrogs
+# Trims space from text.
+
+# Trims whitespace from the left of a string
+function trim_lhs()
+{
+  local text=$1
+  echo "${text#${text%%[! ]*}}"
+}
+
+# Trims whitespace on the right of a string.
+function trim_rhs()
+{
+  local text=$1
+  echo "${text%${text##*[! ]}}"  # 1[! ] => not space
+}
+
+function trim()
+{
+  local text=$1
+  text=$(trim_lhs($text))
+  text=$(trim_rhs($text))
+  echo $text
+}
