ebadc7f7b8221d9de193aa0c0e13094500723ec1
tannerj <codingfrogs@yahoo.com>
2022-10-11 20:42:29 +0100

    docs: fix errors in trim lhs&rhs examples

--- sections/01-learning/01-basics/basics-31-string-trim-lhs.sh
+++ sections/01-learning/01-basics/basics-31-string-trim-lhs.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 # Bash-easy by Codefrogs
-# Show how to trim whitespace.
-# All we use here are the shell pattern matching (not regex).
-# Using x=${x#"${x%%[! ]*}"}
+# Show how to trim LHS (Left Hand Side) whitespace.
+# All we use here is shell pattern matching (not regex).
+
+# To trim on the LHS, we use: x=${x#"${x%%[! ]*}"}
 #
+# This jumble of characters looks complicated, but we go through it step by step.
+#
+# The first step, is to remove the characters to the right of the whitepace.
+#
+# This may seem like we got things the wrong way around but,
+# to do the LHS trim, we need to know what's on the right hand side (RHS) first.
+#
+# This is a trivial example, but explains the principle.
+# For example, suppose we have the string:
+# ' car'  (with one white space to the left)
+#
+# To trim the white space, we create a copy of just the white space.
+# We do that by removing everything that isn't whitespace.
+# In this case it's 'car'.
+#
+# That leaves us with just ' '. The whitespace we want to remove.
+#
+# Let's say,
+# white=' '
+# Now we remove 'white' from our original string ' car'.
+#
+# Then finally we have the left trimmed string: 'car'.
+#
+# The examples below should help make it clearer!
+
 set -u
 echo "Bash-easy by Codefrogs"
 echo
@@ -11,27 +37,26 @@ echo
 format="%d: %s: '%s' - %s\n"
 
 i=1  # Line counter
-line="kaapecatnap abc aaU   "  # Text with three spaces to the left
 line="   Uaa cba pantacepaak"  # Text with three spaces to the left
 
 echo "Trimming LHS"
 echo "------------"
-echo "      1234567890123456789012"
+echo "      1234567890123456789012" # Columns
 echo "line='$line'"
 echo
 
-right_chop="${line%k}"  # Remove first match of 'c'
+right_chop="${line%k}"  # Remove first match of 'k'
 printf "$format" $i "\${line%k}" "$right_chop" "Removes the first character."
 i=$((++i))
 
 # Trying to remove last 'a'. But has no effect as the first character isn't an a
-right_chop="${line%a*}"  # Match anything + 'a'
+right_chop="${line%a*}"  # Reads match any character + 'a'
 printf "$format" $i "\${line%a*}" "$right_chop" "First matching an 'a'!"
 i=$((++i))
 
 # Trying to remove last 'a'. But there is ONLY one 'a'. We need a star.
 right_chop="${line%%a}"  # Match as many 'a's to the left
-printf "$format" $i "\${line%%a}" "$right_chop" "No match: Needs a's to the right."
+printf "$format" $i "\${line%%a}" "$right_chop" "No match: 'k' != 'a'."
 i=$((++i))
 
 # Trying to remove last 'a'. But 'a' does not match 'c'.
@@ -40,34 +65,33 @@ printf "$format" $i "\${line%%a*}" "$right_chop" "Matches up to the last 'a'!"
 i=$((++i))
 
 # Match anything not 'a'
-right_chop="${line%%[!a]}"
+right_chop="${line%%[!a]}" # Reads, match anything not 'a' from the right.
 printf "$format" $i "\${line%%[!a]}" "$right_chop" "Only a match with the k (!=a)"
 i=$((++i))
 
 # Match anything not 'a'
-right_chop="${line%%[!a]*}"
+right_chop="${line%%[!a]*}" # [!a]* Reads, from the right match any characters + not an 'a'.
+# However in this case the '*' eats up all the characters. And the last character is also not an 'a'!
 printf "$format" $i "\${line%%[!a]*}" "$right_chop" "Empty - Matches with last spaces (which are not a)!"
 i=$((++i))
 
 # Match anything not a space
-right_chop="${line%%[! ]*}"  # [! ] => not space
+# In this example we find the spaces we want to remove.
+right_chop="${line%%[! ]*}"  # [! ] Reads, from the right match any characters + non-spaces
 printf "$format" $i "\${line%%[! ]*}" "$right_chop" "Three spaces!"
 i=$((++i))
 
-# echo
-# echo "Compare with:"
-# # Match anything not 'a'
-# echo "      12345678901"
-# text="human   aaa"
-# echo "text='$text'"
-# result="${text##*[!a]}"  # Match anything up to anything not 'a'
-# printf "$format" $i "'$text' with ##*[!a]" "$result" "Matches up to anything not 'a'!"
-# i=$((++i))
-
-# echo
-# echo "Trim spaces left"
+# Trim spaces from the left.
 # Match anything not a space
-right_chop="${line%%[! ]*}"  # [! ] => not space
-trim_left="${line#$right_chop}"  # 1[! ] => not space
-printf "$format" $i "\${line#${line%%[! ]*}   }" "$trim_left" "Trims (lhs)"
+# Just a repeat of the last example.
+three_spaces="${line%%[! ]*}"
+
+# Now we use the '#' operator, as we will chop off the spaces from the left.
+trim_left="${line#$three_spaces}"
+
+# We don't have to use the variables above, and can just replace
+# insert the value of 'three_spaces' instead.
+# i.e. "${line#"${line%%[! ]*}"}"
+#
+printf "$format" $i "\${line#\"\${line%%[! ]*}\"}" "$trim_left" "Trims (lhs)"
 i=$((++i))
--- sections/01-learning/01-basics/basics-32-string-trim-rhs.sh
+++ sections/01-learning/01-basics/basics-32-string-trim-rhs.sh
@@ -1,44 +1,54 @@
 #!/bin/bash
 # Bash-easy by Codefrogs
-# Show how to trim whitespace.
-# All we use here are the shell pattern matching (not regex).
-# Using x=${x#"${x%%[! ]*}"}
+# Show how to trim RHS whitespace.
+# All we use here is shell pattern matching (not regex).
+#
+# To trim on the RHS, we use: rhs_trimmed=${x#"${x%%[! ]*}"}
+#
+# This jumble of characters looks complicated, but we go through it step by step.
 #
 set -u
 echo "Bash-easy by Codefrogs"
 echo
 
-format="%d: %s: '%s' - %s\n"
+format="%d: %s: '%s' - %s\n"  # Ignore this, just for displaying results.
 
 i=1  # Line counter
-line="kaapecatnap abc aaU   "  # Text with three spaces to the left
+line="kaapecatnap abc aaU   "  # Text with three spaces to the right
 
 echo "Trimming RHS"
 echo "------------"
-echo "      1234567890123456789012"
+echo "      1234567890123456789012" # Columns
 echo "line='$line'"
 echo
 
-left_chop="${line#k}"  # Remove first match of 'c'
+# Notice we use the format:
+# ${variable#<pattern>}
+# Where 'pattern' is made of of a set of characters.
+# Remember that the star character '*' matches zero or more characters.
+
+left_chop="${line#k}"  # Remove first match of 'k'
 printf "$format" $i "\${line#k}" "$left_chop" "Removes the first character."
 i=$((++i))
 
-# Trying to remove first 'a'. But has no effect as the first character isn't an a
-left_chop="${line#*a}"  # Match anything + 'a'
+# The pattern '*a'should be read as: match something or nothing (*), then a.
+# This pattern matches 'ka'.
+#
+left_chop="${line#*a}"  # Reads, match any character + 'a'
 printf "$format" $i "\${line#*a}" "$left_chop" "First matching an 'a'!"
 i=$((++i))
 
-# Trying to remove first 'a'. But there is ONLY one 'a'. We need a star.
-left_chop="${line##a}"  # Match as many 'a's to the left
-printf "$format" $i "\${line##a}" "$left_chop" "No match: Needs a's to the left."
+# Trying to remove first 'a'. But 'k' != ''a'.
+left_chop="${line##a}"  # Reads, match as many 'a's to the left
+printf "$format" $i "\${line##a}" "$left_chop" "No match: 'k' != 'a'."
 i=$((++i))
 
-# Trying to remove first 'a'. But 'a' does not match 'c'.
+# The pattern '##*a' reads match as any characters to the left, plus one 'a'.
 left_chop="${line##*a}"  # This is our left chop to 'a'
 printf "$format" $i "\${line##*a}" "$left_chop" "Matches up to the last 'a'!"
 i=$((++i))
 
-# Match anything not 'a'
+# The pattern '##[!a]' reads, match anything not 'a'.
 left_chop="${line##[!a]}"
 printf "$format" $i "\${line##[!a]}" "$left_chop" "Only a match with the k (!=a)"
 i=$((++i))
@@ -53,20 +63,21 @@ left_chop="${line##*[! ]}"  # [! ] => not space
 printf "$format" $i "\${line##*[! ]}" "$left_chop" "Three spaces!"
 i=$((++i))
 
-# echo
-# echo "Compare with:"
-# # Match anything not 'a'
-# echo "      12345678901"
-# text="human   aaa"
-# echo "text='$text'"
-# result="${text##*[!a]}"  # Match anything up to anything not 'a'
-# printf "$format" $i "'$text' with ##*[!a]" "$result" "Matches up to anything not 'a'!"
-# i=$((++i))
-
-# echo
-# echo "Trim spaces right"
+# Trim spaces right
 # Match anything not a space
-left_chop="${line##*[! ]}"  # [! ] => not space
-trim_right="${line%$left_chop}"  # 1[! ] => not space
-printf "$format" $i "\${line%${line##*[! ]}   }" "$trim_right" "Trims (rhs)"
+# Just a repeat of the last example.
+three_spaces="${line##*[! ]}"  # reads, match anything not a space.
+
+# Now we use the '%' operator, as we will chop off the spaces from the right.
+trim_right="${line%$three_spaces}"  # Reads remove from the left the text
+
+# We don't have to use the variables above, and can just replace
+# insert the value of 'three_spaces' instead.
+# i.e. "${line%"${line##*[! ]}"}"
+#
+printf "$format" $i "\${line%\"\${line##*[! ]}\"}" "$trim_right" "Trims (rhs)"
 i=$((++i))
+
+# Notice we have quotes around the text pattern.
+trimmed=${line%"${line##*[! ]}"}
+echo "$i: Final check: '$trimmed'"
