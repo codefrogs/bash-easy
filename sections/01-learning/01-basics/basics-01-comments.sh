6dca56ce119e10a7e27e9567978f2872da72c695
tannerj <codingfrogs@yahoo.com>
2023-03-06 08:08:29 +0000

    feat: Add comments example

---
+++ sections/01-learning/01-basics/basics-01-comments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Bash-easy by Codefrogs
+# A comment line.
+# Another comment!
+# see: www.gnu.org/software/bash
+# Or just type 'help' in the command line!
+#
+echo "We use '#' for comments."
