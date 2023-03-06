1d9b9d947749e2a51465caedc82e8b9af9824176
tannerj <codingfrogs@yahoo.com>
2023-03-06 08:07:21 +0000

    docs: Remove clutter from helloworld

--- sections/01-learning/01-basics/basics-01-helloworld.sh
+++ sections/01-learning/01-basics/basics-01-helloworld.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# Bash-easy by Codefrogs
-# A comment: This line above should already be the very first line!
-# Another comment!
-# see: www.gnu.org/software/bash
-#
-echo "Bash-easy by Codefrogs"
 echo "Hello World!"
