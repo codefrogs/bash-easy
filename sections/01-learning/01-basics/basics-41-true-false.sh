044f37867831ca5cffd4aa2cfa431f77ab64f25b
tannerj <codingfrogs@yahoo.com>
2023-03-14 22:08:10 +0000

    feat: Add true-false return value testing example

--- sections/01-learning/01-basics/basics-41-true-false.sh
+++ sections/01-learning/01-basics/basics-41-true-false.sh
@@ -3,6 +3,9 @@
 echo "Bash-easy by Codefrogs"
 echo
 
+# The intent of this script is to make sure you understand
+# how to use the return values.
+
 result=false
 if [ $result == false ]; then
   echo '1: $result = false :('
@@ -25,22 +28,52 @@ else
   echo 'X: FAIL!'
 fi
 
+# But notice how this fails!
+result=notacommand
+if $result; then
+  echo 'X: Sorry!'
+else
+  echo '4: Error on test'
+fi
+
 result=false
 if $result; then
   echo 'X: PASS!'
 else
-  echo '4: FAIL!'
+  echo '5: FAIL!'
 fi
 
 # An even simpler way perhaps
 result=true
-$result && echo "5: PASS"
+$result && echo "6: PASS"
 
 result=false
 $result && echo "X: PASS"
 
 result=false
-$result || echo "6: FAIL!"
+$result || echo "7: FAIL!"
 
 result=true
 $result || echo "X: FAIL!"
+
+function some_good_case()
+{
+  return 0
+}
+
+function always_fails_case()
+{
+  return 2 
+}
+
+if (some_good_case); then
+  echo "8: Something is OK"
+fi
+
+if (always_fails_case); then
+  echo "X: No error"
+fi
+
+if some_good_case; then
+  echo "9: Final test result"
+fi
