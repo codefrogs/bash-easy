7fe14709d3adeaf0414ae0560bdfdcc13b62efc6
tannerj <codingfrogs@yahoo.com>
2024-06-27 17:31:51 +0100

    chore: Add new lines in exit-codes example

--- sections/01-learning/01-basics/basics-39-exit-codes.sh
+++ sections/01-learning/01-basics/basics-39-exit-codes.sh
@@ -21,28 +21,36 @@ USER_1=1  # User defined exit code (user code range is: 1-125)
 #
 echo "Look at exit code variable \$?"
 echo
+
 false # effectively saying our last command result is 'false'
 echo "1: exit code of (false) \$? = $?"
 echo
+
 true # effectively saying out last command result is 'true'
 echo "2: exit code of (true) \$? = $?"
 echo
+
 ls $0  # Where $0 is the name of this file.
 echo "3: 'ls' known file, exit code \$? = $?" # = SUCCESS
 echo
+
 ls ./this.is.a.unknown.filename
 echo "4: 'ls' unknown file, exit code \$? = $?"  # = CMD_NOT_FOUND
 echo
+
 ./data/empty-file.txt
 echo "5: run data/empty-file.txt, exit code \$? = $?"  # = CMD_NOT_EXECUTABLE
 # The text file isn't executable
 echo
+
 ls -V
 echo "6: 'ls' with bad option, exit code \$? = $?"  # = SERIOUS
 # Do have a look at ls --help
 echo
+
 date
 echo "7: Known command (date), exit code: $?"
 echo
+
 notacommand
 echo "8: Unknown command, exit code: $?"  # = CND_NOT_FOUND
